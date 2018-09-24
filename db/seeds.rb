13bc7d6dea9d3cb743bd52352227e684db46f93e
Cynthia <ex09190919@yahoo.com.tw>
2018-09-24 15:56:42 +0800

    add categtry on db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "admin@example.com", password: "12345678", role: "admin")
+puts "Default admin created"
+
+Category.destroy_all
+  category_list = [
+    { name: "beauty" },
+    { name: "makeup" },
+    { name: "clothes" },
+    { name: "shoes" }
+  ]
+  category_list.each  do |category|
+    Category.create(name: category[:name])
+  end
+  puts "Category created!"
