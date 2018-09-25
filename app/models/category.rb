2202c703e725a9e299755e15a8b900bb4e2bd843
Cynthia <ex09190919@yahoo.com.tw>
2018-09-25 08:39:19 +0800

    setup relation among post,post_category and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :post_categories, dependent: :restrict_with_error
+  has_many :posted_categories, through: :post_categories, source: :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has_many :post_categories, dependent: :restrict_with_error
+  has_many :posted_categories, through: :post_categories, source: :category
+
 end
