963aa6c3f236715c37822490a17869ec37c71d35
Cynthia <ex09190919@yahoo.com.tw>
2018-09-26 07:09:29 +0800

    add new and create  post action ,route and new page for public user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,9 @@ class PostsController < ApplicationController
   def edit
     # set_post
   end
+  def new
+    @post = Post.new
+  end
   def update
     if @post.update(post_params)
       flash[:notice] = "Post was successfully updated"
@@ -17,6 +20,16 @@ class PostsController < ApplicationController
     else
       flash.now[:alert] = "Post was failed to update"
       render :edit
+    end
+  end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:notice] = "Posted successfully"
+      redirect_to post_path
+    else
+      flash.now[:alert] = "Posted faield"
+      render :index
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :show, :edit, :update]
+  resources :posts, only: [:index, :show, :edit, :update, :new, :create]
   root "posts#index"
   namespace :admin do
     root "posts#index"
