2b186950e8768754612b8b1b85df0d5103e742db
Cynthia <ex09190919@yahoo.com.tw>
2018-09-30 09:48:04 +0800

    Modify relation among post and categroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   has_many :post_categories, dependent: :restrict_with_error
-  has_many :posted_categories, through: :post_categories, source: :post
+  has_many :category_posts, through: :post_categories, source: :post
 end
