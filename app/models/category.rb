3c8600e4d65dffdcf95b209c5c8e94ab3956962b
Cynthia <ex09190919@yahoo.com.tw>
2018-10-02 11:26:58 +0800

    Modify post chose category action, model, new and edit page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
   has_many :post_categories, dependent: :restrict_with_error
-  has_many :category_posts, through: :post_categories, source: :post
+  has_many :posts, through: :post_categories, source: :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many :post_categories, dependent: :restrict_with_error
-  has_many :posted_categories, through: :post_categories, source: :category
+  has_many :categories, through: :post_categories, source: :category
   validates_presence_of :title
 # carrierwave照片上傳器
 mount_uploader :image, PhotoUploader
