b5b41cad1abf86e5b5b83917b52560154eca2320
Cynthia <ex09190919@yahoo.com.tw>
2018-10-03 11:50:02 +0800

    Add form on index page for creating new category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   has_many :post_categories, dependent: :restrict_with_error
   has_many :posts, through: :post_categories, source: :post
+  validates_presence_of :name
 end
