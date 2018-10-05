f8d4f187d82df3e347e4b3babecb7279a03ca829
Cynthia <ex09190919@yahoo.com.tw>
2018-10-05 17:22:27 +0800

    User only can delete own comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_post_comment, only: [:create, :destroy]
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user = current_user
     @comment.save!
     redirect_to post_path(@post)
   end
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user = current_user
+      @comment.destroy
+      redirect_to post_path(@post)
+    end
+  end
 
   private
   def comment_params
     params.require(:comment).permit(:content)
+  end
+  def set_post_comment
+    @post = Post.find(params[:post_id])
   end
 end
