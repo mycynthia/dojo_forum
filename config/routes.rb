f9ab7e93f2c0abdd9bd564815f0d7d93644d51f1
Cynthia <ex09190919@yahoo.com.tw>
2018-10-05 08:25:00 +0800

    Setup resources comments for public user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
     root "posts#index"
+    resources :comments, only: [:create, :update, :destroy]
   namespace :admin do
     resources :categories
     resources :users, only: [:index, :update]
