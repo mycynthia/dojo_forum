1d67a1441d2bd78e4b3075512116971e308350df
Cynthia <ex09190919@yahoo.com.tw>
2018-10-08 15:59:41 +0800

    User can sort classified posts by replies_count , last_replied_time and viewed_count

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @category = Category.find(params[:id])
-    @posts = @category.posts.page(params[:page]).per(9)
+    @q = @category.posts.ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(10)
   end
 end
