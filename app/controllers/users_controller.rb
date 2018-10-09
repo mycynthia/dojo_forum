45da80746186e65c3d37c16d3eb2f96ac3a19626
Cynthia <ex09190919@yahoo.com.tw>
2018-10-09 13:13:26 +0800

    Add my comments page and action on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :mycomments]
+
   def show
+    @posts = @user.posts.order(created_at: :desc)
+  end
+  def mycomments
+    @comments = @user.comments.page(params[:page]).per(20)
+    if params[:c_id]
+      @comment = Comment.find(params[:c_id])
+    else
+      @comment = Comment.new
+    end
+  end
+
+  private
+
+  def set_user
     @user = User.find(params[:id])
-    @posts = @user.posts.all
   end
 end
