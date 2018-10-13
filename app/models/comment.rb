a86b89e933570e7552bc5809dc01cc24304a7e4d
Cynthia <ex09190919@yahoo.com.tw>
2018-10-13 11:19:03 +0800

    Add feeds action and page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   validates_presence_of :content
-  belongs_to :user
+  belongs_to :user, counter_cache: :user_count
   belongs_to :post, counter_cache: :replies_count
 end
